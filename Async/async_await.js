b0ad1a428fd6469347a834e4721c6c34dd5ef157
JH-Jitu <jitu000166@gmail.com>
2022-01-11 19:12:22 +0600

    Asynchronous js- promise, async-await updated

--- Async/async_await.js
+++ Async/async_await.js
@@ -1,7 +1,8 @@
-async function hello(params) {
-  return "hello world!";
+async function hello() {
+  return "hello world!"; // hello function ta "hello world" likha take promise akare return korbe 'async' use korar karone
 }
 
+// Jehetu hello() function ekhon ekta promise return kore tai nicher system e amake value ta pete hobe.. jehetu await 'async' function er baire kaaj korena tai .then diyei function er parameter er maddhome value ta pawa jabe
 hello().then(function (value) {
   console.log(value);
 });
